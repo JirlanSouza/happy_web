95102734013fafa74f46fbd2bf71eb7c8221acf8
Jirlan Souza <jirlansouza08@gmail.com>
2020-10-14 22:31:14 -0400

    Adicionado icone dentro do mapa da page OrphanagesMap e também o popup do icone.

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {FiPlus} from 'react-icons/fi'
-import {Map, TileLayer} from 'react-leaflet'
+import {FiPlus, FiArrowRight} from 'react-icons/fi';
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanagesMap.css';
-import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/leaflet.css';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [48, 58],
+    iconAnchor: [24, 58],
+    popupAnchor: [174, 13],
+})
 
 function OrphanagesMap() {
     return (
@@ -34,6 +42,18 @@ function OrphanagesMap() {
                 }}
             >
                 <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <Marker 
+                position={[-15.5970940, -56.1153441]}
+                icon={mapIcon}>
+                    <Popup closeButton={false} minWidth={240} className="map-popup">
+                        Casa de Happy e Kids.
+
+                        <Link to="">
+                            <FiArrowRight size={20} color="#FFF" />
+                        </Link>
+                    </Popup>
+                </Marker>
+                
             </Map>
 
             <Link to="" className="create-orphanage">
