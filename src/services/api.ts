a485f6a18db97f9b2533eabc7e8e021a32a1f0d2
Jirlan Souza <jirlansouza08@gmail.com>
2023-08-05 20:45:32 -0400

    refactor: change the api base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://happy-be.herokuapp.com/',
+    baseURL: "https://happy-backend-3c6ab.rj.r.appspot.com/",
 });
 
 export default api;
